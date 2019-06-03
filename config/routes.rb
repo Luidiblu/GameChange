d7213b3db1fa5a4d540bd54c58225f3eacadbb93
Ricardo Ribeiro <ricardoc6r8@gmail.com>
2019-06-03 17:18:43 -0300

    initial routing for Lobbies

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :lobbies, except: %i[index]
+
+  resources :games do
+    resources :lobbies, only: %i[index]
+  end
+
 end
