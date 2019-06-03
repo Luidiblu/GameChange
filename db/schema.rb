75b833e35cb934fbe938a7294b0b6b396ccf7cbc
Diego Pisani <diego.luidiblu@gmail.com>
2019-06-03 17:05:07 -0300

    Container in application

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_06_03_190241) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
