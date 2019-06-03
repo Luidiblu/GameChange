4548ecc2b808c50351322de0930b620a1c1b2c95
Ricardo Ribeiro <ricardoc6r8@gmail.com>
2019-06-03 17:47:20 -0300

    Seeded =)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,68 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+system 'clear'
+puts "Seed Start"
+
+def separate
+  puts "\n" + "="*100 + "\n" + "\n"
+end
+
+separate
+
+puts "Cleaning database"
+
+Game.destroy_all
+puts "#{Game.count} games!"
+
+User.destroy_all
+puts "#{User.count} users!"
+
+Lobby.destroy_all
+puts "#{Lobby.count} lobbies!"
+
+Session.destroy_all
+puts "#{Session.count} sessions!"
+
+separate
+
+puts "Generating League of Virginity\n\n"
+
+lol = Game.new(
+  name: "League of Legends",
+  photo: "https://logodownload.org/wp-content/uploads/2014/09/lol-logo-league-of-legends.png"
+)
+
+if lol.save
+  puts "---#{lol.name} was created!"
+else
+  puts "=========================== Something went wrong =========================="
+  puts "=================== #{lol.errors.messages} ==================="
+end
+
+lol2 = Game.new(
+  name: "League of Legends",
+  photo: "https://logodownload.org/wp-content/uploads/2014/09/lol-logo-league-of-legends.png"
+)
+
+if lol2.save
+  puts "---#{lol2.name} was created!"
+else
+  puts "=========================== Something went wrong =========================="
+  puts "=================== #{lol2.errors.messages} ==================="
+end
+
+separate
+
+puts "\nEnded!\n\n"
+puts '⣿⣿⣿⣿⣿⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿
+⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿
+⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠙⠿⠿⠿⠻⠿⠿⠟⠿⠛⠉⠀⠀⠀⠀⠀⣸⣿
+⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣴⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⠀⣭⣷⠀⠀⠀⠸⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠈⠉⠀⠀⠤⠄⠀⠀⠀⠉⠁⠀⠀⠀⠀⢿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡀⠉⠀⠀⠀⠀⠀⢄⠀⢀⠀⠀⠀⠀⠉⠉⠁⠀⠀⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿'
