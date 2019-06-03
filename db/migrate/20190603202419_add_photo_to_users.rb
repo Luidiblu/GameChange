2b18bd669f7e0757c2dbceafe02a8f366ed08b1c
Diego Pisani <diego.luidiblu@gmail.com>
2019-06-03 17:26:23 -0300

    New migration profile_pic nd banner_pic

--- db/migrate/20190603202419_add_photo_to_users.rb
+++ db/migrate/20190603202419_add_photo_to_users.rb
@@ -1,5 +1,6 @@
 class AddPhotoToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :photo, :string
+    add_column :users, :profile_pic, :string
+    add_column :users, :banner_pic, :string
   end
 end
