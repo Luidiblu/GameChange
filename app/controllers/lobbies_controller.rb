311a735c24a488b6ea15b3ee9b43d5c5f5afe50f
Ricardo Ribeiro <ricardoc6r8@gmail.com>
2019-06-04 16:31:34 -0300

    Having form issues

--- app/controllers/lobbies_controller.rb
+++ app/controllers/lobbies_controller.rb
@@ -13,11 +13,23 @@ class LobbiesController < ApplicationController
   end
 
   def create
+    @lobby = Lobby.new(lobby_params)
+    @game = @lobby.game
+
+    if @lobby.save
+      redirect_to @game
+    else
+      render :new
+    end
   end
 
   private
 
   def set_game
     @game = Game.find(params[:game_id])
+  end
+
+  def lobby_params
+    params.require(:lobby).permit(:game_id, :competitive, :active)
   end
 end
