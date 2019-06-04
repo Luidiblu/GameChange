326a188179fe5112a9ff36ef999e6f86b8fce59d
Lucas Souza <ibmstemp@gmail.com>
2019-06-04 18:49:51 -0300

    devise user super controller

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,21 @@
+class ProfileController < ApplicationController
+  before_action :authenticate_profile!, except: %i[index show]
+  before_action :set_profile, only: %i[show]
+
+  def show
+    @games.select(:user_id == @user)
+     # @avatar = @user.avatar.url
+
+  end
+
+private
+
+  def set_profile
+      @user = User.find(params[:id])
+  end
+
+  def profile_params
+    params.require(:user).permit(:name, :nickname, :banner_img, :avatar)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: {
+        registrations: 'users/registrations'
+      }
+
   root to: 'pages#home'
 
-  resources :users, only: %i[show]
+  resources :profile, only: %i[show]
 
   resources :games
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
