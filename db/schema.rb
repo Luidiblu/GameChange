cda3a71c85417db149d23b5dec0e34c05dd62226
Ricardo Ribeiro <ricardoc6r8@gmail.com>
2019-06-04 18:45:20 -0300

    Er

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_04_200623) do
+ActiveRecord::Schema.define(version: 2019_06_04_214130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_06_04_200623) do
     t.datetime "updated_at", null: false
     t.bigint "game_id"
     t.string "description"
+    t.integer "max_players"
     t.index ["game_id"], name: "index_lobbies_on_game_id"
   end
 
