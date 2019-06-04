0b60ce5664e715fff19ef03438817446fa0189bf
Diego Pisani <diego.luidiblu@gmail.com>
2019-06-04 11:54:42 -0300

    sla

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2019_06_03_202419) do
 
-ActiveRecord::Schema.define(version: 2019_06_03_190241) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "games", force: :cascade do |t|
     t.string "name"
@@ -49,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_06_03_190241) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "nickname"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
