663182305dbc76f917385d0c072d3174d8fb036d
Diego Pisani <diego.luidiblu@gmail.com>
2019-06-05 15:38:45 -0300

    landingpagewallpaperdone

--- app/javascript/components/typedscript.js
+++ app/javascript/components/typedscript.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const bannerText = () => {
   new Typed('#typed-text', {
-    strings: ["Stress Less.", "Play Better.", "Find Players.", "Have fun!"],
+    strings: ["Find Players...", "Play Better...", "Have fun!"],
     typeSpeed: 50,
     loop: true,
     backDelay: 1100,
