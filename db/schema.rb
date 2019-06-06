329d33b129a4a14c45b3cbfec72746eb136c9966
Ricardo Ribeiro <ricardoc6r8@gmail.com>
2019-06-06 11:11:05 -0300

    ue

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,8 @@ ActiveRecord::Schema.define(version: 2019_06_05_185021) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "nickname"
-    t.string "photo"
+    t.string "profile_pic"
+    t.string "banner_pic"
     t.string "banner_img"
     t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
