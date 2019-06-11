f9f15911844bb7ef80fd0ede6bb3b0aec64b6c36
Ricardo Ribeiro <ricardoc6r8@gmail.com>
2019-06-11 11:48:18 -0300

    I aint the sharpest tool in the sheeeedddd

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :lobby
+
+  validates :content, presence: true, allow_blank: false
+
+  def from?(an_user)
+    an_user == self.user
+  end
 end
