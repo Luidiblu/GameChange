fe72e0b7c7dadda9a131c508151b43bc9eccf3ca
Ricardo Ribeiro <ricardoc6r8@gmail.com>
2019-06-12 11:39:24 -0300

    The ice we skate is getting pretty thin

--- app/models/lobby.rb
+++ app/models/lobby.rb
@@ -9,6 +9,8 @@ class Lobby < ApplicationRecord
   validates :description, presence: true
   validates :max_players, presence: true
 
+  before_create :set_session_token
+
   def user_allowed?(user)
     # TODO
 
@@ -27,5 +29,13 @@ class Lobby < ApplicationRecord
   def move_admin
     self.user = self.sessions.find_by(active: true).user
     self.save
+  end
+
+  def set_session_token
+    opentok = OpenTok::OpenTok.new ENV['OPENTOK_API_KEY'], ENV['OPENTOK_SECRET_API_KEY']
+    session = opentok.create_session :media_mode => :routed
+
+    self.session_token = session.session_id
+    self.generated_token = session.generate_token
   end
 end
